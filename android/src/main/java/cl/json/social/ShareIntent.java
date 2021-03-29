052c7f10c89e48c8e54a304ea99d2afdc8c73a28
paul at flixar <pzhou@lixar.com>
2021-03-29 17:18:44 -0400

    use Intent.EXTRA_TITLE  to set title right for Android 10+

--- android/src/main/java/cl/json/social/ShareIntent.java
+++ android/src/main/java/cl/json/social/ShareIntent.java
@@ -108,6 +108,7 @@ public abstract class ShareIntent {
 
         if (ShareIntent.hasValidKey("title", options)) {
             this.chooserTitle = options.getString("title");
+            this.getIntent().putExtra(Intent.EXTRA_TITLE, this.chooserTitle);
         }
 
         String message = "";
@@ -341,7 +342,6 @@ public abstract class ShareIntent {
             chooser = Intent.createChooser(this.getIntent(), this.chooserTitle);
         }
         chooser.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        chooser.putExtra(Intent.EXTRA_TITLE, this.chooserTitle);
 
         if (ShareIntent.hasValidKey("showAppsToView", options) && ShareIntent.hasValidKey("url", options)) {
             Intent viewIntent = new Intent(Intent.ACTION_VIEW);
